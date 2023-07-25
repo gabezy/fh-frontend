9bfbbd3e6de80ac43c61f21a1bef6711da19e6ed
gabezy <gabrielmoreira2705@gmail.com>
2023-07-25 01:22:58 -0300

    feature: UserLayout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
+import { UserLayoyt } from "./layouts/UserLayoyt";
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
+      <Route path="/home" element={<UserLayoyt />}>
+        <Route path="/home" element={<Home />} />
+      </Route>
     </Routes>
   );
 };
